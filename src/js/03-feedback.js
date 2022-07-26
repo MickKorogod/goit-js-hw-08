793016af2a4c696a68eac7f52efadaaa9c4e9d04
MickKorogod <korogod1995@gmail.com>
2022-07-27 00:45:37 +0300

    tsk3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,32 @@
+import throttle from 'lodash.throttle';
+
+const feedbackForm = document.querySelector('.feedback-form')
+
+function setItem (event) {
+    event.preventDefault()
+    const { email, message } = event.currentTarget;
+    const data = { email: email.value, message: message.value}
+    localStorage.setItem( "feedback-form-state", JSON.stringify(data))
+}
+feedbackForm.addEventListener('input', throttle( setItem, 500))
+
+function updateInput() {
+    if (localStorage.getItem("feedback-form-state") === null) {
+        return
+    }
+    checkInput = JSON.parse(localStorage.getItem("feedback-form-state"))
+    feedbackForm.elements.email.value = checkInput.email;
+    feedbackForm.elements.message.value = checkInput.message;
+}
+ updateInput()
+
+function submitForm(event) {
+     event.preventDefault()
+    const { email, message } = event.currentTarget;
+    const data = { email: email.value, message: message.value }
+    console.log(data)
+    localStorage.removeItem("feedback-form-state")
+    feedbackForm.reset()
+}
+feedbackForm.addEventListener('submit', submitForm)
 
