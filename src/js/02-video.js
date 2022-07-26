b2d98edfc5c38580d1149509d0915a1dc18225ae
MickKorogod <korogod1995@gmail.com>
2022-07-26 23:39:24 +0300

    tsk2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,11 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+const iframe = document.querySelector('iframe')
+const player = new Player(iframe);
 
+const onPlay = ({ seconds }) => {
+    localStorage.setItem("videoplayer-current-time", seconds)
+}
+const getItem = localStorage.getItem("videoplayer-current-time")
+player.setCurrentTime( getItem)
+player.on('timeupdate', throttle (onPlay, 500))
